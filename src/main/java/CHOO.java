aa16ec306b5fe26e1dcb09d127a0ea0a23c06c05
ClarenceChoo <clarencechoo1111@gmail.com>
2026-08-21 02:35:39 +0800

    Changed name to CHOO and added greetings

---
+++ src/main/java/CHOO.java
@@ -0,0 +1,21 @@
+/**
+ * Entry point for the CHOO chatbot.
+ */
+public class CHOO {
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = "##### #   # ##### #####\n"
+                + "#     #   # #   # #   #\n"
+                + "#     ##### #   # #   #\n"
+                + "#     #   # #   # #   #\n"
+                + "##### #   # ##### #####\n";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm CHOO.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
