2262da887c9e9fb8110ddd44704f7cec3a5914a1
ClarenceChoo <clarencechoo1111@gmail.com>
2026-08-21 02:41:02 +0800

    echo commands entered by the user, exit when type 'bye'

--- src/main/java/CHOO.java
+++ src/main/java/CHOO.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the CHOO chatbot.
  */
@@ -15,7 +17,18 @@ public class CHOO {
         System.out.println("Hello! I'm CHOO.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
