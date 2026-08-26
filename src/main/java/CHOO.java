6ac2bbacb6e82bd5e00e2d9986051accfaf4f314
ClarenceChoo <clarencechoo1111@gmail.com>
2026-08-26 22:57:17 +0800

    Extract task list operations
    Encapsulate collection ownership and one-based position validation in TaskList. Convert CHOO into an instance coordinator over Ui, Parser, TaskList, and Storage while retaining rollback on save failures.

--- src/main/java/CHOO.java
+++ src/main/java/CHOO.java
@@ -1,17 +1,32 @@
 import java.nio.file.Path;
-import java.util.List;
 
 /**
  * Entry point for the CHOO chatbot.
  */
 public class CHOO {
+    private final Storage storage;
+    private final Ui ui;
+    private TaskList tasks;
+
+    /**
+     * Creates CHOO with its persistence and user-interface collaborators.
+     *
+     * @param storage storage used to load and save tasks
+     * @param ui user interface used for console interaction
+     */
+    public CHOO(Storage storage, Ui ui) {
+        this.storage = storage;
+        this.ui = ui;
+    }
+
     /**
      * Starts the chatbot and processes commands until the user exits.
      *
      * @param args command-line arguments; not used
      */
     public static void main(String[] args) {
-        run(new Storage(Path.of("data", "choo.txt")));
+        Storage storage = new Storage(Path.of("data", "choo.txt"));
+        new CHOO(storage, new Ui()).run();
     }
 
     /**
@@ -20,62 +35,61 @@ public class CHOO {
      * @param storage storage used to load and save tasks
      */
     public static void run(Storage storage) {
-        Ui ui = new Ui();
-        ui.showWelcome();
+        new CHOO(storage, new Ui()).run();
+    }
 
-        List<Task> tasks;
+    /**
+     * Processes commands until input ends or the user exits.
+     */
+    public void run() {
+        this.ui.showWelcome();
+
         try {
-            tasks = storage.load();
+            this.tasks = new TaskList(this.storage.load());
         } catch (ChooException exception) {
-            ui.showError(exception.getMessage());
+            this.ui.showError(exception.getMessage());
             return;
         }
 
-        while (ui.hasNextCommand()) {
-            String command = ui.readCommand();
+        while (this.ui.hasNextCommand()) {
+            String command = this.ui.readCommand();
             try {
-                if (executeCommand(command, tasks, storage, ui)) {
+                if (executeCommand(Parser.parse(command))) {
                     break;
                 }
             } catch (ChooException exception) {
-                ui.showError(exception.getMessage());
+                this.ui.showError(exception.getMessage());
             }
         }
     }
 
-    private static boolean executeCommand(String command, List<Task> tasks,
-                                          Storage storage, Ui ui)
-            throws ChooException {
-        ParsedCommand parsedCommand = Parser.parse(command);
+    private boolean executeCommand(ParsedCommand parsedCommand) throws ChooException {
         switch (parsedCommand.getType()) {
         case BYE:
-            ui.showBye();
+            this.ui.showBye();
             return true;
         case LIST:
-            ui.showTaskList(tasks);
+            this.ui.showTaskList(this.tasks.asList());
             return false;
         case MARK:
-            updateTaskStatus(parsedCommand.getTaskNumber(), tasks, storage, ui, true);
+            updateTaskStatus(parsedCommand.getTaskNumber(), true);
             return false;
         case UNMARK:
-            updateTaskStatus(parsedCommand.getTaskNumber(), tasks, storage, ui, false);
+            updateTaskStatus(parsedCommand.getTaskNumber(), false);
             return false;
         case DELETE:
-            deleteTask(parsedCommand.getTaskNumber(), tasks, storage, ui);
+            deleteTask(parsedCommand.getTaskNumber());
             return false;
         case ADD:
-            addTask(tasks, parsedCommand.getTask(), storage, ui);
+            addTask(parsedCommand.getTask());
             return false;
         default:
             throw new AssertionError("Unhandled command type: " + parsedCommand.getType());
         }
     }
 
-    private static void updateTaskStatus(int taskNumber, List<Task> tasks, Storage storage,
-                                         Ui ui,
-                                         boolean isMarking) throws ChooException {
-        validateTaskNumber(taskNumber, tasks);
-        Task task = tasks.get(taskNumber - 1);
+    private void updateTaskStatus(int taskNumber, boolean isMarking) throws ChooException {
+        Task task = this.tasks.get(taskNumber);
         boolean wasDone = task.isDone();
         if (isMarking) {
             task.markAsDone();
@@ -83,7 +97,7 @@ public class CHOO {
             task.markAsNotDone();
         }
         try {
-            storage.save(tasks);
+            this.storage.save(this.tasks.asList());
         } catch (ChooException exception) {
             if (wasDone) {
                 task.markAsDone();
@@ -92,39 +106,28 @@ public class CHOO {
             }
             throw exception;
         }
-        ui.showTaskStatusChanged(task, isMarking);
+        this.ui.showTaskStatusChanged(task, isMarking);
     }
 
-    private static void deleteTask(int taskNumber, List<Task> tasks, Storage storage,
-                                   Ui ui) throws ChooException {
-        validateTaskNumber(taskNumber, tasks);
-        Task removedTask = tasks.remove(taskNumber - 1);
+    private void deleteTask(int taskNumber) throws ChooException {
+        Task removedTask = this.tasks.remove(taskNumber);
         try {
-            storage.save(tasks);
+            this.storage.save(this.tasks.asList());
         } catch (ChooException exception) {
-            tasks.add(taskNumber - 1, removedTask);
+            this.tasks.add(taskNumber, removedTask);
             throw exception;
         }
-        ui.showDeletedTask(removedTask, tasks.size());
+        this.ui.showDeletedTask(removedTask, this.tasks.size());
     }
 
-    private static void validateTaskNumber(int taskNumber, List<Task> tasks)
-            throws ChooException {
-        if (taskNumber < 1 || taskNumber > tasks.size()) {
-            throw new ChooException(
-                    "Task number " + taskNumber + " is outside the list.");
-        }
-    }
-
-    private static void addTask(List<Task> tasks, Task task, Storage storage,
-                                Ui ui) throws ChooException {
-        tasks.add(task);
+    private void addTask(Task task) throws ChooException {
+        this.tasks.add(task);
         try {
-            storage.save(tasks);
+            this.storage.save(this.tasks.asList());
         } catch (ChooException exception) {
-            tasks.remove(tasks.size() - 1);
+            this.tasks.remove(this.tasks.size());
             throw exception;
         }
-        ui.showAddedTask(task, tasks.size());
+        this.ui.showAddedTask(task, this.tasks.size());
     }
 }
